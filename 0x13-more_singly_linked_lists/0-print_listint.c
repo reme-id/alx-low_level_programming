0a5e263059af194e6f488f315b5c95020ba97dd4
David Ikharo <daveikharo@yahoo.com>
2023-04-04 15:19:31 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,23 +7,17 @@
  *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
-
 {
+	size_t nodes = 0;
 
-size_t nodes = 0;
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
 
-while (h)
+	}
 
-{
-
-nodes++;
-printf("%d\n", h->n);
-h = h->next;
-
-}
-
-return (nodes);
-
+	return (nodes);
 }
